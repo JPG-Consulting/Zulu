ba18fbf01517f7fe33e84041528b054b102f0ef3
JPG-Consulting <opensource@jpg-consulting.com>
2019-06-15 12:57:55 +0200

    first commit

---
+++ Zulu/RuntimeException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Zulu.Resources;
+
+namespace Zulu
+{
+    public class RuntimeException : SystemException
+    {
+        public RuntimeException() 
+            : base(CommonResources.RuntimeException_Generic)
+        { }
+
+        public RuntimeException(String message) 
+            : base(message)
+        { }
+
+        public RuntimeException(String message, Exception innerException) 
+            : base(message, innerException)
+        { }
+
+        protected RuntimeException(SerializationInfo info, StreamingContext context) 
+            : base(info, context)
+        { }
+    }
+}
