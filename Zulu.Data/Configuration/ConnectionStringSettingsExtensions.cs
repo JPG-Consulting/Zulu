59561b4a4f56769ccf5117082a774c640a054c16
JPG-Consulting <opensource@jpg-consulting.com>
2019-06-16 11:40:22 +0200

    Changes

--- Zulu.Data/Configuration/ConnectionStringSettingsExtensions.cs
+++ Zulu.Data/Configuration/ConnectionStringSettingsExtensions.cs
@@ -1,25 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data.Common;
-using System.Linq;
-using System.Text;
 
 namespace Zulu.Configuration
 {
     public static class ConnectionStringSettingsExtensions
     {
-        public static DbProviderFactory GetDbProviderFactory(this ConnectionStringSettings connectionStringSettings)
+        public static DbCommand CreateDbCommand(this ConnectionStringSettings connectionStringSettings)
         {
             if (connectionStringSettings == null)
                 throw new ArgumentNullException("connectionStringSettings");
 
-            // ToDo: Validaciones
+            DbConnection connection = connectionStringSettings.CreateDbConnection();
+            DbCommand command = connection.CreateCommand();
 
-            return DbProviderFactories.GetFactory(connectionStringSettings.ProviderName);
+            if (command.Connection == null)
+            {
+                command.Connection = connection;
+            }
+
+            return command;
         }
 
-        public static DbConnection CreateConnection(this ConnectionStringSettings connectionStringSettings)
+        public static DbConnection CreateDbConnection(this ConnectionStringSettings connectionStringSettings)
         {
             if (connectionStringSettings == null)
                 throw new ArgumentNullException("connectionStringSettings");
@@ -27,10 +30,45 @@ namespace Zulu.Configuration
             DbProviderFactory providerFactory = connectionStringSettings.GetDbProviderFactory();
             DbConnection connection = providerFactory.CreateConnection();
 
-
-            connection.ConnectionString = connectionStringSettings.ConnectionString;
+            // Establecemos la cadena de conexión.
+#if NET35
+            if ((connectionStringSettings.ConnectionString != null) && (connectionStringSettings.ConnectionString.Trim().Length > 0))
+#else
+            if (!String.IsNullOrWhitespace(connectionStringSettings.ConnectionString))
+#endif
+                connection.ConnectionString = connectionStringSettings.ConnectionString;
 
             return connection;
+        }
+
+        /// <summary>
+        ///   Obtiene la factoría del proveedor de datos especificado en la cadena de conexión.
+        /// </summary>
+        /// <param name="connectionStringSettings">La configuración de la cadena de conexión.</param>
+        /// <returns><see cref="DbProviderFactory"/> relacionado con la cadena de conexión.</returns>
+        public static DbProviderFactory GetDbProviderFactory(this ConnectionStringSettings connectionStringSettings)
+        {
+            if (connectionStringSettings == null)
+                throw new ArgumentNullException("connectionStringSettings");
+
+            // ToDo: Validaciones
+#if NET35
+            if ((connectionStringSettings.ProviderName == null) || (connectionStringSettings.ProviderName.Trim().Length == 0))
+#else
+            if (String.IsNullOrWhitespace(connectionStringSettings.ProviderName))
+#endif
+                throw new ConfigurationErrorsException(
+                    "",
+                    connectionStringSettings.ElementInformation.Source,
+                    connectionStringSettings.ElementInformation.LineNumber);
+
+            if (DbProviderFactories.GetFactoryClasses().Rows.Find(connectionStringSettings.ProviderName) == null)
+                throw new ConfigurationErrorsException(
+                    "",
+                    connectionStringSettings.ElementInformation.Source,
+                    connectionStringSettings.ElementInformation.LineNumber);
+
+            return DbProviderFactories.GetFactory(connectionStringSettings.ProviderName);
         }
     }
 }
--- Zulu/RuntimeException.cs
+++ Zulu/RuntimeException.cs
@@ -6,18 +6,96 @@ namespace Zulu
 {
     public class RuntimeException : SystemException
     {
+        /// <summary>
+        ///   Inicializa una nueva instancia de la clase <see cref="RuntimeException"/>.
+        /// </summary>
+        /// <remarks>
+        ///   <para>
+        ///     Este constructor inicializa la propiedad <see cref="Exception.Message"/> de la nueva 
+        ///     instancia en un mensaje proporcionado por el sistema que describe el error y tiene en 
+        ///     cuenta la referencia cultural del sistema actual.
+        ///   </para>
+        ///   <para>
+        ///     Todas las clases derivadas deben proporcionar este constructor predeterminado.
+        ///   </para>
+        /// </remarks>
         public RuntimeException() 
             : base(CommonResources.RuntimeException_Generic)
         { }
 
+        /// <summary>
+        ///   Inicializa una nueva instancia de la clase <see cref="RuntimeException"/> con el 
+        ///   mensaje de error especificado.
+        /// </summary>
+        /// <param name="message">
+        ///   Mensaje que describe el error.
+        /// </param>
+        /// <remarks>
+        ///   <para>
+        ///     Este constructor inicializa la propiedad <see cref="Exception.Message"/> de la 
+        ///     nueva instancia utilizando el parámetro <paramref name="message"/>. Si el parámetro 
+        ///     <paramref name="message"/>es <c>null</c>, esto equivale a llamar al constructor
+        ///     <see cref="RuntimeException.RuntimeException"/>.
+        ///   </para>
+        /// </remarks>
         public RuntimeException(String message) 
             : base(message)
         { }
 
+        /// <summary>
+        ///   Inicializa una nueva instancia de la clase <see cref="RuntimeException"/> con el mensaje 
+        ///   de error especificado y una referencia a la excepción interna que representa la causa de 
+        ///   esta excepción.
+        /// </summary>
+        /// <param name="message">
+        ///   Mensaje de error que explica el motivo de la excepción.
+        /// </param>
+        /// <param name="innerException">
+        ///   La excepción que es la causa de la excepción actual o una referencia nula (<c>Nothing</c> 
+        ///   en Visual Basic) si no se especifica ninguna excepción interna.
+        /// </param>
+        /// <remarks>
+        ///   <para>
+        ///     Una excepción que se produce como consecuencia directa de una excepción anterior debe 
+        ///     incluir una referencia a esta última en la propiedad <see cref="Exception.InnerException"/>. 
+        ///     La propiedad <see cref="Exception.InnerException"/> devuelve el mismo valor pasado al 
+        ///     constructor, o una referencia nula (<c>Nothing</c> en Visual Basic) si la propiedad 
+        ///     <see cref="Exception.InnerException"/> no proporciona el valor de la excepción interna 
+        ///     al constructor.
+        ///   </para>
+        /// </remarks>
         public RuntimeException(String message, Exception innerException) 
             : base(message, innerException)
         { }
 
+        /// <summary>
+        ///   Inicializa una nueva instancia de la clase <see cref="RuntimeException"/> con datos 
+        ///   serializados.
+        /// </summary>
+        /// <param name="info">
+        ///   <see cref="SerializationInfo"/> que contiene los datos serializados del objeto que 
+        ///   hacen referencia a la excepción que se va a producir.
+        /// </param>
+        /// <param name="context">
+        ///   <see cref="StreamingContext"/> que contiene información contextual sobre el origen 
+        ///   o el destino.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   <para>
+        ///     <paramref name="info"/> es <c>null</c>.
+        ///   </para>
+        /// </exception>
+        /// <exception cref="SerializationException">
+        ///   <para>
+        ///     El nombre de clase es <c>null</c> o <see cref="System.Exception.HResult"/> es cero (0).
+        ///   </para>
+        /// </exception>
+        /// <remarks>
+        ///   <para>
+        ///     Durante la deserialización se llama a este constructor para reconstituir el objeto de 
+        ///     excepción transmitido en una secuencia.
+        ///   </para>
+        /// </remarks>
         protected RuntimeException(SerializationInfo info, StreamingContext context) 
             : base(info, context)
         { }
